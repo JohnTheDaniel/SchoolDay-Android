8d3f99e24727f9de232db11a762e65d8dc3c3446
John Daniel <daniel@bosser.com>
2015-01-07 23:19:01 +0100

    gitignore files gone

---
+++ SchoolDayAndroidApp2/app/src/main/java/com/bosser/schoolday/AddNovaScheduleActivity.java
@@ -0,0 +1,47 @@
+package com.bosser.schoolday;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+import com.bosser.schoolday.R;
+
+public class AddNovaScheduleActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_nova_schedule);
+
+        WebView novaScriptRunner = (WebView) findViewById(R.id.nova_script_runner);
+        novaScriptRunner.getSettings().setJavaScriptEnabled(true);
+        novaScriptRunner.setWebChromeClient(new WebChromeClient());
+        novaScriptRunner.loadUrl("javascript:alert('Hello World!')");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_nova_schedule, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
